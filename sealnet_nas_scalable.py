4cf79afe023d28b4fb9ec3a1fe110b2a693d2767
bentocg <bentocollares@gmail.com>
2018-03-16 16:35:22 -0400

    deleted some old files and added excel spreadsheet output to blob detector

--- sealnet_nas_scalable.py
+++ sealnet_nas_scalable.py
@@ -770,7 +770,7 @@ def main():
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=1, gamma=0.99)
 
     # start training
-    model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=2)
+    model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=100)
 
 
 if __name__ == '__main__':
