ce276dd304d8b3551726c8edbc89236c613ad143
bentocg <bentocollares@gmail.com>
2018-03-22 17:25:28 -0400

    added TODO list to get a working function

--- create_trainingset.py
+++ create_trainingset.py
@@ -15,13 +15,14 @@ def get_patches(raster_dir, vector_df, lon, lat, patch_sizes, labels):
         lon : column in vector_df containing longitude component.
         lat : column in vector_df containing latitude component.
         patch_sizes : list with pyramid dimensions in multi-band output, first element is used as base dimension,
-            subsequent elements will be downsampled to match patch_sizes[0]
+            subsequent elements must be larger than the previous element and will be re-sized to match patch_sizes[0]
         labels : list with training set labels
 
     Output:
         folder with extracted multi-band training images separated in subfolders by intended label
     """
-
+    #TODO: check for increasing size in patch_sizes
+    #TODO: create output directory on current path and subdirectories based on training labels
     rasters = []
     for path, _, files in os.walk(raster_dir):
         for filename in files:
@@ -43,33 +44,19 @@ def get_patches(raster_dir, vector_df, lon, lat, patch_sizes, labels):
         x0, y0, w, h = gt[0], gt[3], gt[1], gt[5]
 
         data = band.ReadAsArray().astype(numpy.float)
+        #TODO: add padding to raster data based on the last element patch_sizes
         # free up memory
         del gdata
+        # iterate through the points
+        for p in vector_df.iterrows():
+            x = int((p[1][lon] - x0)/w)
+            y = int((p[1][lat] - y0)/h)
+            for scale in patch_sizes:
+                try:
+                   if data[y, x] != nodata:
+                      #TODO: save patch image to correct subfolder based on label
+                      data[y, x]
+                except:
+                    pass
 
-        if i == 0:
-            # iterate through the points
-            for p in vector_df.iterrows():
-                x = int((p[1][lon] - x0)/w)
-                Xc = x0 + x*w + w/2  # the cell center x
-                y = int((p[1][lat] - y0)/h)
-                Yc = y0 + y*h + h/2  # the cell center y
-                try:
-                    if data[y, x] != nodata:
-                        presVAL = [p[1][lon], p[1][lat], '{:.6f}'.format(Xc), '{:.6f}'.format(Yc), data[y, x]]
-                        presValues.append(presVAL)
-                except:
-                    pass
-            df = pandas.DataFrame(presValues, columns=['x', 'y', 'Xc', 'Yc', rs])
-        else:
-            # iterate through the points
-            for p in vector_df.iterrows():
-                x = int((p[1][lon] - x0)/w)
-                y = int((p[1][lat] - y0)/h)
-                try:
-                    if data[y, x] != nodata:
-                        presValues.append(data[y, x])
-                except:
-                    pass
-            df[rs] = pandas.Series(presValues)
-    del data, band
-    return df
+        del data, band
