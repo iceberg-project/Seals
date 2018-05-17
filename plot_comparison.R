4b7f24a9fde839e900c3398b0ee0ca4dfa334de5
bentocg <bentocollares@gmail.com>
2018-05-17 15:23:52 -0400

    added counting pipeline and fixed plotting scripts to be more general

--- plot_comparison.R
+++ plot_comparison.R
@@ -3,25 +3,36 @@
 # load packages
 library(ggplot2)
 library(reshape2)
-library(argparser)
+library(argparse)
 
 # define arg-parser 
-parser = arg_parser("R script to get validation stats and plot a confusion matrix")
-parser = add_argument(parser, "input_file", help=".csv file with precision and recall")
-parser = add_argument(parser, "output_file", help="filename for the .png plot")
+parser = ArgumentParser(description="R script to get pooled data csv and plot model comparisons")
+parser$add_argument("--input_file", type="character", help=".csv file with precision and recall")
+parser$add_argument("--output_file", type="character", help='filename for the .png plot')
+parser$add_argument("--x", type="character", help="name of the column which will be used as the X axis")
+parser$add_argument("--y", type="character", help='name of the column which will be used as the Y axis')
+parser$add_argument("--facet", type="character", help='name of the column which will be used to determine facets')
+args = parser$parse_args()
 
 # unroll arguments
 args = parse_args(parser=parser)
 inp_file = args$input_file
 out_file = args$output_file
+x_label = args$x
+y_label = args$y
+facet = args$facet
 
 # read csv table from all 
 pooled_data = read.csv(inp_file, stringsAsFactors=FALSE)
-colnames(pooled_data)[2] = 'label'
+
+# add columns for plot axes and facet wrap
+pooled_data['x'] = pooled_data[, which(colnames(pooled_data) == x_label)]
+pooled_data['y'] = pooled_data[, which(colnames(pooled_data) == y_label)]
+pooled_data['facet'] =pooled_data[, which(colnames(pooled_data) == facet)]
 
 # function for plotting precision/recall of a single label
 label_plot = ggplot(data=pooled_data, 
-                    mapping=aes(x=recall * 100, y=precision * 100, color=model_name)) +
+                    mapping=aes(x=x, y=y, color=model_name)) +
     theme_minimal(base_size=15) +
     geom_point(size=4, alpha=0.8) +
     ylim(c(0,100)) +
@@ -30,7 +41,7 @@ label_plot = ggplot(data=pooled_data,
     theme(axis.title = element_text(face="bold", size=18),
           strip.text.x = element_text(size=18, face="italic"))+
     scale_colour_brewer(palette="Set1") +
-    facet_wrap(~label, ncol=2, scales="free") 
+    facet_wrap(~facet, ncol=2, scales="free") 
 
 # save confusion_matrix as a png figure
 png(out_file, width=800, height=800)
