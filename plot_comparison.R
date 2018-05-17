ab3b59f989e00711eaad3e1a0cbd419761fc7d3c
bentocg <bentocollares@gmail.com>
2018-05-17 16:33:53 -0400

    fixed a few things with plotting scripts

--- plot_comparison.R
+++ plot_comparison.R
@@ -11,11 +11,10 @@ parser$add_argument("--input_file", type="character", help=".csv file with preci
 parser$add_argument("--output_file", type="character", help='filename for the .png plot')
 parser$add_argument("--x", type="character", help="name of the column which will be used as the X axis")
 parser$add_argument("--y", type="character", help='name of the column which will be used as the Y axis')
-parser$add_argument("--facet", type="character", help='name of the column which will be used to determine facets')
-args = parser$parse_args()
+parser$add_argument("--facet", default='NULL', type="character", help='name of the column which will be used to determine facets')
 
 # unroll arguments
-args = parse_args(parser=parser)
+args = parser$parse_args()
 inp_file = args$input_file
 out_file = args$output_file
 x_label = args$x
@@ -30,20 +29,27 @@ pooled_data['x'] = pooled_data[, which(colnames(pooled_data) == x_label)]
 pooled_data['y'] = pooled_data[, which(colnames(pooled_data) == y_label)]
 pooled_data['facet'] =pooled_data[, which(colnames(pooled_data) == facet)]
 
+# find x and y limits
+x_lim = c(0, max(pooled_data['x']))
+y_lim = c(0, max(pooled_data['y']))
+
 # function for plotting precision/recall of a single label
 label_plot = ggplot(data=pooled_data, 
                     mapping=aes(x=x, y=y, color=model_name)) +
     theme_minimal(base_size=15) +
     geom_point(size=4, alpha=0.8) +
-    ylim(c(0,100)) +
-    xlim(c(0,100)) +
-    labs(x='Recall', y='Precision') +
+    labs(x=x_label, y=y_label) +
+    xlim(x_lim) +
+    ylim(y_lim) +
     theme(axis.title = element_text(face="bold", size=18),
           strip.text.x = element_text(size=18, face="italic"))+
-    scale_colour_brewer(palette="Set1") +
-    facet_wrap(~facet, ncol=2, scales="free") 
+    scale_colour_brewer(palette="Set1") 
+
+if(facet != 'NULL'){
+    label_plot = label_plot + facet_wrap(~facet, ncol=2, scales="free")
+}
 
 # save confusion_matrix as a png figure
-png(out_file, width=800, height=800)
+png(out_file, width=1200, height=800)
 print(label_plot)
 dev.off()
