7dba60ad847e43c0ec2aa146c38dbcf5649ff06b
bentocg <bentocollares@gmail.com>
2018-05-31 12:13:04 -0400

    Latest runs. remove saved_models for .gitignore

--- get_mse.R
+++ get_mse.R
@@ -11,6 +11,7 @@ library(argparse)
 parser = ArgumentParser(description="R script to get validation stats for counting CNNs")
 parser$add_argument("--input_file", type="character", help="model name generated during training")
 parser$add_argument("--pipeline", type="character", help='model pipeline')
+parser$add_argument("--dest_folder", type="character", default="saved_models", help="folder where pipeline is located")
 args = parser$parse_args()
 
 
@@ -49,7 +50,7 @@ get_mse = function(csv_file){
                      'precision'=precision, 'recall'=recall, 'total_predicted'=sum(rows[,'predicted']), 
                      'total_ground_truth'=sum(rows[,'ground_truth']))
     
-    write.csv(mse, glue("./saved_models/{pipeline}/{model_name}/{model_name}_mse.csv"))
+    write.csv(mse, glue("./{dest_folder}/{pipeline}/{model_name}/{model_name}_mse.csv"))
 }
 
 
@@ -57,7 +58,8 @@ get_mse = function(csv_file){
 # run for validation data
 model_name = args$input_file
 pipeline = args$pipeline
-get_mse(csv_file=glue('./saved_models/{pipeline}/{model_name}/{model_name}_validation.csv'))
+dest_folder = args$dest_folder
+get_mse(csv_file=glue('./{dest_folder}/{pipeline}/{model_name}/{model_name}_validation.csv'))
 
 
 
