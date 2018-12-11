0f2ad37c26182ea2d46177c4a1864da51852c92f
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2018-12-11 16:30:41 -0500

    Breaking output folder structure

--- src/tiling/tile_raster.py
+++ src/tiling/tile_raster.py
@@ -1,18 +1,22 @@
-import numpy as np
-import rasterio
 import os
 import argparse
-import cv2
 import time
-
+import cv2
+import rasterio
+import numpy as np
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='divides a raster image into files')
-    parser.add_argument('--scale_bands', type=str, help='for multi-scale models, string with size of scale bands '
-                                                        'separated by spaces')
-    parser.add_argument('--input_image', type=str, help='full path to raster file we wish to tile out')
-    parser.add_argument('--output_folder', type=str, help='folder where tiles will be stored')
-    parser.add_argument('--pad', type=str, default=False, help='flag for padding the image, required for multiscale')
+    parser = argparse.ArgumentParser(description='Divides a raster image into \
+                                     files')
+    parser.add_argument('--scale_bands', type=str, help='For multi-scale \
+                        models, string with size of scale bands separated \
+                        by spaces')
+    parser.add_argument('--input_image', type=str, help='Full path to raster \
+                        file we wish to tile out')
+    parser.add_argument('--output_folder', type=str, help='Folder where tiles \
+                        will be stored')
+    parser.add_argument('--pad', type=str, default=False, help='Flag for \
+                        padding the image, required for multiscale')
 
     return parser.parse_args()
 
@@ -52,7 +56,8 @@ def tile_raster(input_image, output_folder, scales, pad_img=False):
             down = y + scales[0] // 2
             right = x + scales[0] // 2
             for scale in scales:
-                curr_scale = band[x - scale // 2: x + scale // 2,  y - scale // 2: y + scale // 2]
+                curr_scale = band[x - scale // 2: x + scale // 2,
+                                  y - scale // 2: y + scale // 2]
                 curr_scale = cv2.resize(curr_scale, (scales[0], scales[0]))
                 scale_bands.append(curr_scale)
             # remove black corners
@@ -61,12 +66,14 @@ def tile_raster(input_image, output_folder, scales, pad_img=False):
             # combine scales and save tile
             scale_bands = np.dstack(scale_bands)
             # save it with polygon coordinates
-            filename = "{}/tile_{}_{}_{}_{}_.jpg".format(output_folder, up, left, down, right)
+            filename = "{}/tile_{}_{}_{}_{}_.jpg".format(output_folder, up,
+                                                         left, down, right)
             cv2.imwrite(filename, scale_bands)
             count += 1
     toc = time.time()
     elapsed = toc - tic
-    print('\n{} tiles created in {} minutes and {:.2f} seconds'.format(count, int(elapsed // 60), elapsed % 60))
+    print('\n{} tiles created in {} minutes and {:.2f} seconds'.
+          format(count, int(elapsed // 60), elapsed % 60))
 
 
 def main():
@@ -74,16 +81,12 @@ def main():
 
     # unroll arguments
     input_image = args.input_image
-    output_folder = args.output_folder
     scales = [int(ele) for ele in args.scale_bands.split('_')]
-    output_folder = './{}/{}/{}/tiles'.format(output_folder, os.path.basename(input_image), args.scale_bands)
+    output_folder = args.output_folder + '/tiles'
+
 
     tile_raster(input_image, output_folder, scales, True)
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
