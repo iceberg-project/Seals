9b5419213a9c7208830a96afd35d193b45fe04e5
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2019-03-06 07:20:46 -0500

    Connecting everything

--- src/iceberg/seals/predicting/predict_raster.py
+++ src/iceberg/seals/predicting/predict_raster.py
@@ -10,15 +10,16 @@ License: MIT
 Copyright: 2018-2019
 """
 
-#import torch
+import torch
 import warnings
 import argparse
 import os
 import shutil
 import time
 import random
-#from utils.model_library import *
-#from predict_sealnet import predict_patch
+import json
+from ..utils.model_library import *
+from predict_sealnet import predict_patch
 from ..iceberg_zmq import Publisher, Subscriber
 warnings.filterwarnings('ignore', module='PIL')
 
@@ -46,6 +47,9 @@ class SealnetPredict(object):
         self._publisher_in = Publisher(channel=self._name, url=self._in_addr_in)
         self._subscriber_in = Subscriber(channel=self._name, url=self._in_addr_out)
         self._subscriber_in.subscribe(topic=self._name)
+    
+        with open(cfg) as conf:
+            self._cfg = json.loads(conf)
 
     def _connect(self):
 
@@ -71,26 +75,27 @@ class SealnetPredict(object):
 
         return None
 
-    def _predict_raster(self):
-        time.sleep(random.randint(10,30))
-        # # predict tiles
-        # model = model_defs[pipeline][args.model_architecture]
+    def _predict_raster(self, pipeline, model_arch, training_set, model_path,
+                        hyperparameter_set, test_folder, output_folder):
+        #time.sleep(random.randint(10,30))
+        # predict tiles
+        model = model_defs[pipeline][model_arch]
 
-        # use_gpu = torch.cuda.is_available()
-        # if use_gpu:
-        #     model.cuda()
-        # model.eval()
+        use_gpu = torch.cuda.is_available()
+        if use_gpu:
+            model.cuda()
+        model.eval()
 
-        # # load saved model weights from training
-        # model_name = args.model_architecture + '_ts-' + args.training_set.split('_')[-1]
-        # model.load_state_dict(
-        #     torch.load("%s/%s.tar" % (args.model_path, model_name)))
+        # load saved model weights from training
+        model_name = model_arch + '_ts-' + training_set.split('_')[-1]
+        model.load_state_dict(
+            torch.load("%s/%s.tar" % (model_path, model_name)))
 
-        # predict_patch(model=model, input_size=model_archs[args.model_architecture]['input_size'],
-        #           batch_size=hyperparameters[args.hyperparameter_set]['batch_size_test'],
-        #           test_dir=args.test_folder,
-        #           output_dir='%s' % (args.output_folder),
-        #           num_workers=hyperparameters[args.hyperparameter_set]['num_workers_train'])
+        predict_patch(model=model, input_size=model_archs[model_arch]['input_size'],
+                  batch_size=hyperparameters[hyperparameter_set]['batch_size_test'],
+                  test_dir=test_folder,
+                  output_dir='%s' % (output_folder),
+                  num_workers=hyperparameters[hyperparameter_set]['num_workers_train'])
 
     def run(self):
 
@@ -102,9 +107,13 @@ class SealnetPredict(object):
             image = self._get_image()
             print(image)
             if image not in ['disconnect','wait']:
-                self._predict_raster()#input_image=image,
-                #                output_folder=self._output_path,
-                #                scales=self._scale_bands)
+                self._predict_raster(pipeline=self._cfg['pipeline'],
+                                     model_arch=self._cfg['model_arch'],
+                                     training_set=self._cfg['training_set'],
+                                     model_path=self._cfg['model_path'],
+                                     hyperparameter_set=self._cfg['hyperparameter_set'],
+                                     test_folder=image,
+                                     output_folder=self._cfg['output_folder'])
             elif image == 'wait':
                 time.sleep(1)
             else:
@@ -115,31 +124,11 @@ class SealnetPredict(object):
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='validates a CNN at the haul out level')
-    parser.add_argument('--input_image', type=str, help='base directory to recursively search for validation images in')
-    parser.add_argument('--model_architecture', type=str, help='model architecture for seal detection')
-    parser.add_argument('--hyperparameter_set', type=str, help='combination of hyperparameters used for CNNs, must be a '
-                                                           'member of hyperparameters dictionary')
-    parser.add_argument('--training_set', type=str, help='training set where models were trained')
-    parser.add_argument('--test_folder', type=str, default='to_classify', help='folder where the model will be saved')
-    parser.add_argument('--tile', type=bool, default=False, help='Input image needs to be tiled')
-    parser.add_argument('--model_path', type=str, help='folder where the model is tarball is')
-    parser.add_argument('--output_folder', type=str, help='folder where results will be saved')
-    # unroll arguments
+    parser.add_argument('--name', type=str)
+    parser.add_argument('--queue_in', type=str)
+    parser.add_argument('--config_file',type=str)
     args = parser.parse_args()
-    # pipeline = model_archs[args.model_architecture]['pipeline']
-    # input_image = args.input_image
-    # output_folder = args.test_folder
-    # scales = [model_archs[args.model_architecture]['input_size']]
-    
-    # # check if the image needs to be tiled.
-    # if args.tile:
-        
-    #     from tile_raster import tile_raster
 
-    #     # check for pre-existing tiles and subtiles
-    #     if os.path.exists('%s/tiles' % (args.test_folder)):
-    #         shutil.rmtree('%s/tiles' % (args.test_folder))
+    pred = SealnetPredict(name=args.name, queue_in=args.queue_in, cfg=args.config_file)
 
-    #     # tile raster into patches
-    #     tile_raster(input_image, output_folder, scales)
-
+    pred.run()
--- src/iceberg/seals/tiling/tile_raster.py
+++ src/iceberg/seals/tiling/tile_raster.py
@@ -17,7 +17,7 @@ import os
 import argparse
 import time
 import random
-import numpy as np
+# import numpy as np
 import pandas as pd
 import rasterio
 import cv2
@@ -95,64 +95,64 @@ class ImageTilling(object):
 
     def _tile_raster(self, input_image, output_folder, scales, pad_img=True):
 
-        time.sleep(random.randint(10,30))
+        #time.sleep(random.randint(10,30))
         
-        # # time it
-        # tic = time.time()
+        # time it
+        tic = time.time()
 
-        # # read image
-        # with rasterio.open(input_image) as src:
-        #     band = np.array(src.read()[0, :, :], dtype=np.uint8)
-        #     # save affine matrix
-        #     affine_matrix = pd.DataFrame({'transform': src.transform[:6]})
-        #     affine_matrix.to_csv('%s/affine_matrix.csv' % output_folder)
+        # read image
+        with rasterio.open(input_image) as src:
+            band = np.array(src.read()[0, :, :], dtype=np.uint8)
+            # save affine matrix
+            affine_matrix = pd.DataFrame({'transform': src.transform[:6]})
+            affine_matrix.to_csv('%s/affine_matrix.csv' % output_folder)
 
-        # # add tiles subfolder
+        # add tiles subfolder
         output_folder = '%s/%s/' % (output_folder, os.path.basename(input_image))
-        # if not os.path.exists(output_folder):
-        #     os.makedirs(output_folder)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
-        # # pad image
-        # pad = 0
+        # pad image
+        pad = 0
 
-        # if pad_img:
-        #     pad = scales[-1] // 2
-        #     band = np.pad(band, pad_width=pad, mode='constant', constant_values=0)
+        if pad_img:
+            pad = scales[-1] // 2
+            band = np.pad(band, pad_width=pad, mode='constant', constant_values=0)
 
-        # # extract tile size and raster size
-        # tile_size = scales[0]
-        # raster_width = band.shape[0]
-        # raster_height = band.shape[1]
+        # extract tile size and raster size
+        tile_size = scales[0]
+        raster_width = band.shape[0]
+        raster_height = band.shape[1]
 
-        # # tile out image
-        # count = 0
-        # for x in range(tile_size // 2 + pad, raster_width - pad, tile_size):
-        #     for y in range(tile_size // 2 + pad, raster_height - pad, tile_size):
-        #         scale_bands = []
-        #         # find corners for polygon
-        #         up = y - scales[0] // 2
-        #         left = x - scales[0] // 2
-        #         down = y + scales[0] // 2
-        #         right = x + scales[0] // 2
-        #         for scale in scales:
-        #             curr_scale = band[x - scale // 2: x + scale // 2, 
-        #                               y - scale // 2: y + scale // 2]
-        #             curr_scale = cv2.resize(curr_scale, (scales[0], scales[0]))
-        #             scale_bands.append(curr_scale)
-        #         # remove black corners
-        #         if np.max(scale_bands[0]) == 0:
-        #             continue
-        #         # combine scales and save tile
-        #         scale_bands = np.dstack(scale_bands)
-        #         # save it with polygon coordinates
-        #         filename = "%s/tile_%d_%d_%d_%d_.jpg" % (output_folder, up, left, 
-        #                                                  down, right)
-        #         cv2.imwrite(filename, scale_bands)
-        #         count += 1
-        # toc = time.time()
-        # elapsed = toc - tic
-        # print('\n%d tiles created in %d minutes' % (count, int(elapsed // 60)) +
-        #       ' and %.2f seconds' % elapsed % 60)
+        # tile out image
+        count = 0
+        for x in range(tile_size // 2 + pad, raster_width - pad, tile_size):
+            for y in range(tile_size // 2 + pad, raster_height - pad, tile_size):
+                scale_bands = []
+                # find corners for polygon
+                up = y - scales[0] // 2
+                left = x - scales[0] // 2
+                down = y + scales[0] // 2
+                right = x + scales[0] // 2
+                for scale in scales:
+                    curr_scale = band[x - scale // 2: x + scale // 2, 
+                                      y - scale // 2: y + scale // 2]
+                    curr_scale = cv2.resize(curr_scale, (scales[0], scales[0]))
+                    scale_bands.append(curr_scale)
+                # remove black corners
+                if np.max(scale_bands[0]) == 0:
+                    continue
+                # combine scales and save tile
+                scale_bands = np.dstack(scale_bands)
+                # save it with polygon coordinates
+                filename = "%s/tile_%d_%d_%d_%d_.jpg" % (output_folder, up, left, 
+                                                         down, right)
+                cv2.imwrite(filename, scale_bands)
+                count += 1
+        toc = time.time()
+        elapsed = toc - tic
+        print('\n%d tiles created in %d minutes' % (count, int(elapsed // 60)) +
+              ' and %.2f seconds' % elapsed % 60)
 
         self._publisher_out.put(topic='image', msg={'name': self._name,
                                                     'request': 'enqueue',
@@ -180,18 +180,19 @@ class ImageTilling(object):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='divides a raster image into \
                                                   files')
-    parser.add_argument('--scale_bands', type=str, help='for multi-scale models, \
+    parser.add_argument('--name', type=str)
+    parser.add_argument('--scale_bands', type=int, help='for multi-scale models, \
                                                          string with size of \
                                                          scale bands separated \
                                                          by spaces')
-    parser.add_argument('--input_image', type=str, help='full path to raster \
-                                                         file we wish to tile \
-                                                         out')
     parser.add_argument('--output_folder', type=str, help='folder where tiles \
                                                            will be stored')
-    parser.add_argument('--pad', type=str, default=False, help='flag for padding \
-                                                                the image, \
-                                                                required for \
-                                                                multiscale')
+    parser.add_argument('--queue_in', type=str)
+    parser.add_argument('--queue_out', tpye=str)
 
     args = parser.parse_args()
+
+    tiler = ImageTilling(name=args.name, scale_bands=args.scale_bands,
+                        output_path=args.output_folder, queue_in=args.queue_in,
+                        queue_out=args.queue_out)
+    tiler.run()
